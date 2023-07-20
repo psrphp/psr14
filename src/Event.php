4258d270eeb7ce2fea269a4c76cd741620690c02
John Doe <johndoe@example.com>
2023-07-20 10:33:30 +0800

    1

--- src/Event.php
+++ src/Event.php
@@ -4,17 +4,13 @@ declare(strict_types=1);
 
 namespace PsrPHP\Psr14;
 
-use Fig\EventDispatcher\ParameterDeriverTrait;
+use Fig\EventDispatcher\AggregateProvider;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
 use Psr\EventDispatcher\StoppableEventInterface;
-use SplPriorityQueue;
 
-class Event implements EventDispatcherInterface, ListenerProviderInterface
+class Event extends AggregateProvider implements EventDispatcherInterface, ListenerProviderInterface
 {
-    use ParameterDeriverTrait;
-    private $listeners = [];
-
     public function dispatch(object $event)
     {
         if ($event instanceof StoppableEventInterface && $event->isPropagationStopped()) {
@@ -27,27 +23,5 @@ class Event implements EventDispatcherInterface, ListenerProviderInterface
             }
         }
         return $event;
-    }
-
-    public function getListenersForEvent(object $event): iterable
-    {
-        $queue = new SplPriorityQueue();
-        foreach ($this->listeners as $listener) {
-            if ($event instanceof $listener['type']) {
-                $queue->insert($listener['listener'], $listener['priority']);
-            }
-        }
-        return $queue;
-    }
-
-    public function listen(callable $listener, int $priority = 0): self
-    {
-        $type = $this->getParameterType($listener);
-        $this->listeners[] = [
-            'type' => $type,
-            'listener' => $listener,
-            'priority' => $priority,
-        ];
-        return $this;
     }
 }
